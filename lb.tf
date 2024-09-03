1f1b71de53c47b73aa59e08f1caada0971178f59
AouadhiBilel <bilel.aouadhi@gmail.com>
2024-09-03 23:51:51 +0200

    time to sleep

---
+++ lb.tf
@@ -0,0 +1,85 @@
+resource "google_compute_global_address" "alb_global_address" {
+  project = var.project_id
+  name = "${var.prefix}-alb-global-adress"
+  address_type = "EXTERNAL"
+}
+
+resource "google_compute_global_forwarding_rule" "alb_forwarding_rule_https" {
+  project = var.project_id
+  name = "${var.prefix}-alb-forwarding-rule-https"
+  ip_protocol = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range = "443"
+  target = google_compute_target_https_proxy.alb_target_https_proxy.id
+  ip_address = google_compute_global_address.alb_global_address.id
+}
+
+resource "google_compute_global_forwarding_rule" "alb_forwarding_rule_http" {
+  project = var.project_id
+  name = "${var.prefix}-alb-forwarding-rule-http"
+  ip_protocol = "TCP"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  port_range = "80"
+  target = google_compute_target_https_proxy.alb_target_http_proxy.id
+  ip_address = google_compute_global_address.alb.id
+}
+
+resource "google_compute_url_map" "https_redirect_url_map" {
+  project = var.project_id
+  name = "${var.prefix}-https-redirect-url-map"
+  default_url_redirect {
+    https_redirect = true
+    strip_query = false
+    redirect_response_code = "MOVED_PERMANENTLY_DEFAULT"
+  }
+}
+
+resource "google_compute_url_map" "alb_url_map" {
+  project = var.project_id
+  name = "${var.prefix}-alb-url-map"
+  default_service = google_compute_backend_service.private_bucket_backend_svc.id
+
+  header_action {
+    request_headers_to_remove = ["Cookie"]
+  }
+}
+
+resource "google_compute_target_http_proxy" "alb_http_target_proxy" {
+  project = var.project_id
+  name = "${var.prefix}-alb-http-target-proxy"
+  url_map = google_compute_url_map.https_redirect_url_map.id
+
+}
+
+
+resource "google_compute_target_https_proxy" "alb_https_target_proxy" {
+  project = var.project_id
+  name = "${var.prefix}-alb-https-target-proxy"
+  url_map = google_compute_url_map.alb_url_map.id
+  ssl_certificates = var.create_certs ? [google_compute_managed_ssl_certificate.alb_managed_cert.id] : [google_compute_ssl_certificate.self_signed_cert.id] 
+}
+
+resource "google_compute_backend_service" "private_bucket_backend_svc" {
+  project = var.project_id
+  name = "${var.prefix}-private-bucket-backend-svc"
+  load_balancing_scheme = "EXTERNAL_MANAGED"
+  protocol = "HTTPS"
+  enable_cdn = true
+
+  custom_request_headers = [
+    "host: ${google_compute_global_network_endpoint.private_bucket_ne.private_bucket_ne.fqdn}"
+  ]
+
+  backend {
+    group = google_compute_global_network_endpoint_group.private_bucket_neg.id
+  }
+
+  security_settings {
+    aws_v4_authentication {
+      access_key_id = google_storage_hmac_key.private_bucket_hmac_key.access_id
+      access_key = google_storage_hmac_key.private_bucket_hmac_key.secret
+      access_key_version = var.access_key_version
+      origin_region = var.region
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "prefix" {
+  type = string
+  description = "prefix for the resources, needs to be 15 chars or less"
+}
+
+variable "region" {
+  
+}
+
+variable "project_id" {
+  
+}
+
+variable "enable_bucket_versioning" {
+  
+}
+
+variable "create_certs" {
+  type = bool
+}
+
+variable "access_key_version" {
+  
+}
