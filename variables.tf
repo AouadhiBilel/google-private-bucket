690a34ed132b2138bfbd2e758917c083ebb9923b
AouadhiBilel <bilel.aouadhi@gmail.com>
2024-09-04 23:33:52 +0200

    added more resources

--- variables.tf
+++ variables.tf
@@ -4,21 +4,30 @@ variable "prefix" {
 }
 
 variable "region" {
-  
+  type = string
+  description = "The region in which to create GCP resources"
+  default = "europe-west1"
 }
 
 variable "project_id" {
-  
+  type = string
+  description = "The ID of the GCP project"
 }
 
 variable "enable_bucket_versioning" {
-  
+  type = bool
+  description = "Enable or not GCP bucket versioning"
+  default = false
 }
 
 variable "create_certs" {
   type = bool
+  description = "Create or not GCP managed certs"
+  default = true
 }
 
 variable "access_key_version" {
-  
+  type = string
+  description = "Version of HMAC key"
+  default = "latest"
 }
